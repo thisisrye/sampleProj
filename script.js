d1c4cd83f33bb065ad722ad935c2c6862fefe0a7
ririking <janrydiaz1401@gmail.com>
2026-08-27 22:51:58 +0800

    Added Random String Message Function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Popup
 const popup = document.getElementById("popup");
 
 popup.style.display = "flex";
@@ -6,4 +7,25 @@ popup.addEventListener("click", (event) => {
     if (event.target === popup) {
         popup.style.display = "none";
     }
+});
+
+//Random message in array when page reloads
+window.addEventListener("DOMContentLoaded", () => {
+    const messages = [
+        "Pizza",
+        "Burger",
+        "Mango",
+        "Water Bottle",
+        "Spoon",
+        "Fork",
+        "Rice",
+        "Siomai",
+        "Wonton",
+        "Mami",
+        "Noodles"
+    ];
+
+    const randomText = messages[Math.floor(Math.random() * messages.length)];
+
+    document.querySelector(".header-comment").textContent = randomText;
 });
